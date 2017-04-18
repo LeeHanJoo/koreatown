98b5f6af736a33c980a72182bdd7d59162ad2ad2
Chicharito <final486@gmail.com>
2017-04-18 22:22:46 +0900

    4.18 home work

--- app/src/main/java/kr/nt/koreatown/main/SubAct.java
+++ app/src/main/java/kr/nt/koreatown/main/SubAct.java
@@ -1,8 +1,13 @@
 package kr.nt.koreatown.main;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import kr.nt.koreatown.R;
+import kr.nt.koreatown.databinding.MainactBinding;
+import kr.nt.koreatown.databinding.SubactivityBinding;
 
 /**
  * Created by user on 2017-04-18.
@@ -14,6 +19,8 @@ public class SubAct extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+        SubactivityBinding binding = DataBindingUtil.setContentView(this, R.layout.subactivity);
 
     }
 }
