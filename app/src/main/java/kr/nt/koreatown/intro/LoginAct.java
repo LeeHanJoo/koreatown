d0dad1cc11442d01945afbc749922bdba15890a8
LeeHanJoo <final486@gmail.com>
2017-05-08 19:34:37 +0900

    5/8 작업

--- app/src/main/java/kr/nt/koreatown/intro/LoginAct.java
+++ app/src/main/java/kr/nt/koreatown/intro/LoginAct.java
@@ -1,5 +1,6 @@
 package kr.nt.koreatown.intro;
 
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
@@ -9,15 +10,34 @@ import android.support.annotation.Nullable;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Toast;
 
 import com.facebook.login.LoginManager;
+import com.squareup.otto.Subscribe;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
+import kr.nt.koreatown.Common;
+import kr.nt.koreatown.KoreaTown;
 import kr.nt.koreatown.R;
+import kr.nt.koreatown.bus.LoginEvent;
 import kr.nt.koreatown.databinding.LoginactBinding;
+import kr.nt.koreatown.main.MainAct;
+import kr.nt.koreatown.retrofit.RetrofitAdapter;
+import kr.nt.koreatown.retrofit.RetrofitUtil;
+import kr.nt.koreatown.bus.BusProvider;
+import kr.nt.koreatown.util.Utils;
+import kr.nt.koreatown.vo.MsgVO;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by user on 2017-04-20.
@@ -47,7 +67,7 @@ public class LoginAct extends BaseLogin {
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
-
+        BusProvider.getInstance().register(this);
         initUI();
     }
 
@@ -61,6 +81,14 @@ public class LoginAct extends BaseLogin {
         binding.emailLogin.setOnClickListener(signClick);
         binding.facebookLogin.setOnClickListener(signClick);
         binding.kakaoLogin.setOnClickListener(signClick);
+        binding.loginBtn.setOnClickListener(signClick);
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        BusProvider.getInstance().unregister(this);
     }
 
     View.OnClickListener signClick = new View.OnClickListener() {
@@ -68,7 +96,8 @@ public class LoginAct extends BaseLogin {
         public void onClick(View v) {
             switch(v.getId()){
                 case R.id.email_login:
-
+                    Intent intent = new Intent(LoginAct.this,SignEmailAct.class);
+                    startActivity(intent);
                     break;
                 case R.id.facebook_login:
                     LoginManager.getInstance().logInWithReadPermissions(LoginAct.this, Arrays.asList("public_profile", "email"));
@@ -76,10 +105,81 @@ public class LoginAct extends BaseLogin {
                 case R.id.kakao_login:
                     isKakaoLogin();
                     break;
+                case R.id.login_btn:
+                    validation();
+                    break;
             }
         }
     };
 
+    private void validation(){
+        String id = binding.inputId.getText().toString().trim();
+        String password = binding.inputPw.getText().toString().trim();
+
+        if(id.isEmpty() || id.length() == 0){
+            binding.inputId.hasFocus();
+            Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+            binding.inputId.startAnimation(shake);
+            Toast.makeText(this,"아이디를 입력해주세요.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(password.isEmpty() || password.length() == 0){
+            binding.inputPw.hasFocus();
+            Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+            binding.inputPw.startAnimation(shake);
+            Toast.makeText(this,"비밀번호를 입력해주세요.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        sendLogin(id,password);
+    }
+
+    private void sendLogin(String ID,String PASSWORD){
+        String PUSH_KEY = "";
+        String OS_VER = Utils.getOsVersion();
+        String DEVICE_ID = Utils.getAndroidID(this);
+
+        Map<String, RequestBody> params = new HashMap<String, RequestBody>();
+        params.put(Common.ID, RetrofitUtil.toRequestBody(ID));
+        params.put(Common.PASSWORD,RetrofitUtil.toRequestBody(PASSWORD));
+        params.put(Common.PUSH_KEY,RetrofitUtil.toRequestBody(PUSH_KEY));
+        params.put(Common.OS_VER,RetrofitUtil.toRequestBody(OS_VER));
+        params.put(Common.DEVICE_ID,RetrofitUtil.toRequestBody(DEVICE_ID));
+        params.put(Common.LAT,RetrofitUtil.toRequestBody(String.valueOf(KoreaTown.myLocation.getLatitude())));
+        params.put(Common.LON,RetrofitUtil.toRequestBody(String.valueOf(KoreaTown.myLocation.getLongitude())));
+
+        Call<MsgVO> call =  RetrofitAdapter.getInstance().doLogin(params);
+        call.enqueue(new Callback<MsgVO>() {
+            @Override
+            public void onResponse(Call<MsgVO> call, Response<MsgVO> response) {
+                MsgVO item = response.body();
+                if(item != null ){
+                    if(item.getResult().equals("1")){ // 성공
+                        Intent intent = new Intent(LoginAct.this, MainAct.class);
+                        startActivity(intent);
+                        finish();
+                    }else{
+
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MsgVO> call, Throwable t) {
+
+            }
+        });
+    }
+
+    @Subscribe
+    public void LoginComple(LoginEvent event){
+        Toast.makeText(this, "리프레쉬뷰 로그인 버스 들어옴", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(LoginAct.this, MainAct.class);
+        startActivity(intent);
+        finish();
+
+    }
 
 }
 
---
+++ app/src/main/java/kr/nt/koreatown/intro/SignEmailAct.java
@@ -0,0 +1,252 @@
+package kr.nt.koreatown.intro;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import kr.nt.koreatown.Common;
+import kr.nt.koreatown.R;
+import kr.nt.koreatown.bus.BusProvider;
+import kr.nt.koreatown.bus.LoginEvent;
+import kr.nt.koreatown.databinding.SignEmailBinding;
+import kr.nt.koreatown.retrofit.RetrofitAdapter;
+import kr.nt.koreatown.retrofit.RetrofitUtil;
+import kr.nt.koreatown.util.Utils;
+import kr.nt.koreatown.vo.MsgVO;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Created by user on 2017-05-08.
+ */
+
+public class SignEmailAct extends AppCompatActivity{
+
+    SignEmailBinding binding = null;
+    String mSex = "";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.sign_email);
+        binding.toolbar.setTitle("이메일가입");
+        setSupportActionBar(binding.toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        init();
+
+
+    }
+
+    private void init(){
+        binding.nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validation();
+            }
+        });
+
+
+        binding.sexLayout.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                switch(checkedId){
+                    case R.id.radio_1:
+                        mSex = "M";
+                        break;
+                    case R.id.radio_2:
+                        mSex = "W";
+                        break;
+                }
+            }
+        });
+
+    }
+
+    private void validation(){
+        String id = binding.inputId.getText().toString().trim();
+        String emailAddr = binding.inputEmail.getText().toString().trim();
+        String nickName = binding.inputNick.getText().toString().trim();
+        String password = binding.inputPw.getText().toString().trim();
+        String password2 = binding.inputPw2.getText().toString().trim();
+        String birth = binding.inputBirth.getText().toString().trim();
+
+        if(id.isEmpty() || id.length() == 0){
+            binding.inputId.hasFocus();
+            Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+            binding.inputId.startAnimation(shake);
+            Toast.makeText(this,"아이디를 입력해주세요.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(emailAddr.isEmpty() || emailAddr.length() == 0){
+            binding.inputEmail.hasFocus();
+            Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+            binding.inputEmail.startAnimation(shake);
+            Toast.makeText(this,"이메일주소를 입력해주세요.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(!Utils.validEmail(emailAddr)){
+            binding.inputEmail.hasFocus();
+            Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+            binding.inputEmail.startAnimation(shake);
+            Toast.makeText(this,"이메일주소를 정확히 입력해주세요.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(nickName.isEmpty() || nickName.length() == 0){
+            binding.inputNick.hasFocus();
+            Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+            binding.inputNick.startAnimation(shake);
+            Toast.makeText(this,"닉네임을 입력해주세요.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(password.isEmpty() || password.length() == 0){
+            binding.inputPw.hasFocus();
+            Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+            binding.inputPw.startAnimation(shake);
+            Toast.makeText(this,"비밀번호를 입력해주세요.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(password.length() < 6){
+            binding.inputPw.hasFocus();
+            Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+            binding.inputPw.startAnimation(shake);
+            Toast.makeText(this,"비밀번호는 6자리이상 입력하셔야합니다",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(!Utils.Passwrodvalidate(password)){
+            binding.inputPw.hasFocus();
+            Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+            binding.inputPw.startAnimation(shake);
+            Toast.makeText(this,"비밀번호는 6자리이상 15이하 영문,숫자,특수문자를 포함하여야합니다.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(password2.isEmpty() || password2.length() == 0){
+            binding.inputPw2.hasFocus();
+            Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+            binding.inputPw2.startAnimation(shake);
+            Toast.makeText(this,"비밀번호를 한번더 입력해주세요.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(password2.length() < 6){
+            binding.inputPw2.hasFocus();
+            Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+            binding.inputPw2.startAnimation(shake);
+            Toast.makeText(this,"비밀번호는 6자리이상 입력하셔야합니다",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(!Utils.Passwrodvalidate(password2)){
+            binding.inputPw2.hasFocus();
+            Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+            binding.inputPw2.startAnimation(shake);
+            Toast.makeText(this,"비밀번호는 6자리이상 15이하 영문,숫자,특수문자를 포함하여야합니다.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(!password.equals(password2)){
+            binding.inputPw2.hasFocus();
+            Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+            binding.inputPw2.startAnimation(shake);
+            Toast.makeText(this,"비밀번호가 다릅니다. 정확히 입력해주세요.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(birth.isEmpty() || birth.length() == 0){
+            binding.inputBirth.hasFocus();
+            Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+            binding.inputBirth.startAnimation(shake);
+            Toast.makeText(this,"생년월일을 입력해주세요.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(birth.length() < 8){
+            binding.inputBirth.hasFocus();
+            Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+            binding.inputBirth.startAnimation(shake);
+            Toast.makeText(this,"생년월일을 정확히 입력해주세요.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(mSex.isEmpty() || mSex.length() == 0){
+            Toast.makeText(this,"성별을 입력해주세요.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(!binding.termsCheck.isChecked()){
+            Toast.makeText(this,"약관 동의를 체크해주세요.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        sendSign(id,emailAddr,password,nickName,mSex,birth);
+
+    }
+
+
+    private void sendSign(String ID,String EMAIL,String PASSWORD , String NICK_NAME,String SEX,String BIRTHDAY){
+        String PUSH_KEY = "";
+        String OS_VER = Utils.getOsVersion();
+        String DEVICE_ID = Utils.getAndroidID(this);
+
+        Map<String, RequestBody> params = new HashMap<String, RequestBody>();
+        params.put(Common.ID, RetrofitUtil.toRequestBody(ID));
+        params.put(Common.EMAIL,RetrofitUtil.toRequestBody(EMAIL));
+        params.put(Common.PASSWORD,RetrofitUtil.toRequestBody(PASSWORD));
+        params.put(Common.BIRTHDAY,RetrofitUtil.toRequestBody(BIRTHDAY));
+
+        params.put(Common.NICK_NAME,RetrofitUtil.toRequestBody(NICK_NAME));
+        params.put(Common.SEX,RetrofitUtil.toRequestBody(SEX));
+        params.put(Common.PUSH_KEY,RetrofitUtil.toRequestBody(PUSH_KEY));
+
+        params.put(Common.OS_VER,RetrofitUtil.toRequestBody(OS_VER));
+        params.put(Common.DEVICE_ID,RetrofitUtil.toRequestBody(DEVICE_ID));
+
+        params.put(Common.OS_TYPE,RetrofitUtil.toRequestBody(Common.TYPE_OS_ANDROID));
+        params.put(Common.MEMBER_TYPE,RetrofitUtil.toRequestBody(Common.TYPE_EMAIL));
+
+        Call<MsgVO> call = RetrofitAdapter.getInstance().postSign(params);
+        call.enqueue(new Callback<MsgVO>() {
+            @Override
+            public void onResponse(Call<MsgVO> call, Response<MsgVO> response) {
+                MsgVO result = response.body();
+                if(result != null ){
+                    if(result.getResult().equals("1")){ // 성공
+                        BusProvider.getInstance().post(new LoginEvent());
+                        finish();
+                    }else{
+
+                    }
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<MsgVO> call, Throwable t) {
+                Log.e("","");
+            }
+        });
+    }
+}
